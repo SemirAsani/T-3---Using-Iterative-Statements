5347ec77f05c2e30939046f66fb882877104613c
SemirAsani <semirasani@usf.edu>
2022-02-09 22:37:24 -0500

    fix

--- Program.cs
+++ Program.cs
@@ -9,20 +9,20 @@ namespace T_3___Using_Iterative_Statements
             //ask user for input between the 5 and 25 range
             Console.WriteLine("Enter a value between 5 and 25 to see conversions");
             string input = Console.ReadLine();
-            int inputValue = int.Parse(input);
+            int inputVal = int.Parse(input);
             //condition for the user input value to be between 5 and 25
-            if ((inputValue > 5) && (inputValue < 25))
+            if ((inputVal > 5) && (inputVal < 25))
             {
                 //display the first line of the table
                 Console.WriteLine("0 CAD = 0 USD");
                 //while loop to prevent iterations from exceeding 200
-                while (inputValue <= 200)
+                while (inputVal <= 200)
                 {
                     //conversion to American dollars from Canadian
-                    double usd = inputValue * 0.792367;
-                    Console.WriteLine(inputValue.ToString() + " CAD = " + usd + " USD");
+                    double usd = inputVal * 0.792367;
+                    Console.WriteLine(inputVal.ToString() + " CAD = " + usd + " USD");
                     //increments CAD table by 10
-                    inputValue += 10;
+                    inputVal += 10;
                 }
             }
             //else statement for any input scenario that is not a valid number between 5 and 25
